6c6d0eaaed2e398d2976ee7441dffb7068bd7012
httpscammy <109007099+httpscammy@users.noreply.github.com>
2023-10-02 23:55:11 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#my-domain").innerHTML = generateDomainName();
+};
+
+let generateDomainName = () => {
+  let pronoun = ["the", "my", "your", "our"];
+  let adj = ["beautiful", "funny", "small", "loud", "cute"];
+  let noun = ["bunny", "meme", "baker", "worker", "diver"];
+  let domain = [".com", ".org", ".net", ".io", ".us"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let domainIndex = Math.floor(Math.random() * domain.length);
+
+  return (
+    pronoun[pronounIndex] +
+    adj[adjIndex] +
+    noun[nounIndex] +
+    domain[domainIndex]
+  );
 };
